3910e6784e6c0356a158c2fa976712cd19ffa77e
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2025-11-03 14:57:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// METTRE UN H1 
+// PUSH SUR GITHUB
+function App() {
+
+  
+  return (
+// <> balise vide parent
+    <>
+      <h1>Bienvenue sur mon blog</h1>
+     
+    </>
+  )
+}
+
+export default App
